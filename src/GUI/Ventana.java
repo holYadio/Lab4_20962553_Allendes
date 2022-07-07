66d3c1d579766fa16ccfc5024ea2ba8ad533e08c
Jhoisan Allendes <jhoisan.allendes@usach.cl>
2022-07-07 18:30:25 -0400

    Se sigue trabajando en la GUI

--- src/GUI/Ventana.java
+++ src/GUI/Ventana.java
@@ -4,7 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Ventana extends JFrame {
+    public JPanel panel;
     public Ventana(){
+
         super("Dobble Game");
         setSize(400, 500);
         setLocationRelativeTo(null);
@@ -14,18 +16,43 @@ public class Ventana extends JFrame {
         initComponent();
     }
     private void  initComponent() {
+        colocarPaneles();
+        primerosComponents();
+    }
+    private void colocarPaneles(){
         //Panel
-        Color base = new Color(255, 220, 76);
-        JPanel panel1 = new JPanel();
-        panel1.setBackground(base);
-        this.getContentPane().add(panel1);
+        panel = new JPanel();
+        panel.setBackground(new Color(255, 220, 76));
+        this.getContentPane().add(panel);
         //panel1.setLayout(null);
+    }
+
+    private void primerosComponents(){
 
         // etiqueta 1
-        JLabel txtInicio = new JLabel();
-        txtInicio.setText("tuki" );
-        txtInicio.setBounds(10,10,200,20);
+        JLabel txtInicio = new JLabel("Bienvenido al juego Dobble", SwingConstants.CENTER);
         txtInicio.setForeground(new Color(0, 0, 0));
-        panel1.add(txtInicio);
-    }
+        txtInicio.setFont(new Font("roboto",Font.BOLD,15));
+
+
+        // Instruccion 1
+        JLabel inst1 = new JLabel("Para poder jugar ingrese un nombre de usuario a registrar", SwingConstants.CENTER);
+
+        // Espacio para el nuevo usuario
+        JTextField nombreUsuario = new JTextField("Nombre de usuario");
+
+
+        // Boton 1
+        JButton boton1 = new JButton();
+        boton1.setText("Registrar");
+        // Ocupar setEnabled(false); cuando pasemos a la siguiente ventana
+
+        // Boton 2
+        JButton boton2 = new JButton();
+
+        panel.add(txtInicio);
+        panel.add(inst1);
+        panel.add(nombreUsuario);
+        panel.add(boton1);
+        }
 }
